4726b9be81f37cdf5fb485327f3dccab06435b0c
Leomar Padron <leopadron.404@gmail.com>
2026-07-28 21:50:47 -0500

    posible fix en BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Viridian-Forest">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
